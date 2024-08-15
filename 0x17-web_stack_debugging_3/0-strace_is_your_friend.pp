9e697458cda5d9754a8719862c8d45739872f75b
TheAbdurrahamanJamiu <abdurrahamanjamiu75@outlook.com>
2024-08-14 17:14:40 -0700

    puppet

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,29 @@
+# Puppet manifest to fix file permissions and ensure a module is enabled
+
+# Ensure the file has the correct permissions
+file { '/path/to/file':
+  ensure  => 'file',
+  mode    => '0644',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
+
+# Ensure the Apache module is enabled
+apache::mod { 'rewrite':
+  ensure => present,
+}
+
+# Ensure the Apache service is running and enabled
+service { 'apache2':
+  ensure     => 'running',
+  enable     => true,
+  subscribe  => File['/path/to/file'],
+}
+
+# Alternatively, use exec to enable module if not using the puppetlabs/apache module
+exec { 'enable_apache_module':
+  command => '/usr/sbin/a2enmod rewrite',
+  path    => ['/usr/bin', '/usr/sbin'],
+  unless  => '/usr/sbin/a2query -m rewrite | grep -q "enabled"',
+  notify  => Service['apache2'],
+}
